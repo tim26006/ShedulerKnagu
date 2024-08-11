46cf0f52d6c1f45374b12ef6ce8b1d98bb060450
ulibk <timsidorin@gmail.com>
2024-08-11 20:13:15 +1000

    init project

---
+++ main.py
@@ -0,0 +1,5 @@
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
